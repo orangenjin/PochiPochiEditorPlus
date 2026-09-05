0ed2304402ed58a083a839652afd29fc9a2197bc
orangenjin <takachizu4@gmail.com>
2026-09-06 05:52:34 +0900

    add hextextbox

---
+++ PochiPochiEditorPlus/CustomControls/HexTextBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace PochiPochiEditorPlus.CustomControls
+{
+    public class HexTextBox : TextBox
+    {
+        /// <summary>
+        /// 16進数文字のキー入力の判定を行う。
+        /// </summary>
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            // 制御キーを許可
+            if (char.IsControl(e.KeyChar)) return;
+
+            // 入力文字判定
+            if (!Uri.IsHexDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// フォーカスが外れた時、桁数整形を行う。
+        /// </summary>
+        protected override void OnLeave(EventArgs e)
+        {
+            base.OnLeave(e);
+
+            if (string.IsNullOrWhiteSpace(Text)) return;
+
+            if (int.TryParse(Text, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out int value))
+            {
+                Text = value.ToString("X8");
+            }
+            else
+            {
+                Text = string.Empty;
+            }
+        }
+    }
+}
--- PochiPochiEditorPlus/MainForm.Designer.cs
+++ PochiPochiEditorPlus/MainForm.Designer.cs
@@ -30,13 +30,33 @@ namespace PochiPochiEditorPlus
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.hexTextBox1 = new PochiPochiEditorPlus.CustomControls.HexTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // hexTextBox1
+            // 
+            this.hexTextBox1.Location = new System.Drawing.Point(321, 140);
+            this.hexTextBox1.Name = "hexTextBox1";
+            this.hexTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.hexTextBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(933, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hexTextBox1);
             this.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -44,10 +64,14 @@ namespace PochiPochiEditorPlus
             this.Name = "MainForm";
             this.Text = "メイン画面";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private CustomControls.HexTextBox hexTextBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
