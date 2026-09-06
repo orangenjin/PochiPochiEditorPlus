cc1d65c4567d1e2d6275522961848f75a5f78ae3
orangenjin <takachizu4@gmail.com>
2026-09-06 12:12:20 +0900

    event binder

--- PochiPochiEditorPlus/MainForm.cs
+++ PochiPochiEditorPlus/MainForm.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows.Forms;
+
+using PochiPochiEditorPlus._Helpers;
+using PochiPochiEditorPlus._Managers;
+using PochiPochiEditorPlus._Utilities;
 
 namespace PochiPochiEditorPlus
 {
     public partial class MainForm : Form
     {
+        // フォーム同時起動用
+        private FormGroupManager _formGroupManager = null;
+        // イベント登録・解除用
+        private EventBinder _eventBinder = new EventBinder();
+        // 共有データ用
+        private SharedData _sharedData = null;
+        // 変更履歴管理用
+        // private UndoManager _undoManager = new UndoManager();
+
         public MainForm()
         {
             InitializeComponent();
---
+++ PochiPochiEditorPlus/_Utilities/EventBinder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PochiPochiEditorPlus._Utilities
+{
+    public class EventBinder
+    {
+        // イベント解除用
+        private List<Action> _detachActions = new List<Action>();
+
+        /// <summary>
+        /// 通常のイベントの追加と解除。
+        /// </summary>
+        public void BindCtrl(
+            Action<EventHandler> adder,
+            Action<EventHandler> remover,
+            EventHandler handler = null)
+        {
+            // 解除用の時、nullにする
+            if (handler == null)
+            {
+                handler = (_, __) => Dispose();
+            }
+
+            adder(handler);
+            _detachActions.Add(() => remover(handler));
+        }
+
+        /// <summary>
+        /// 自作のイベントの追加と解除。
+        /// </summary>
+        public void BindCustom(Action attachAction, Action detachAction)
+        {
+            attachAction();
+            _detachActions.Add(detachAction);
+        }
+
+        /// <summary>
+        /// 破棄のタイミングを指定する用。
+        /// </summary>
+        public void Dispose()
+        {
+            foreach (var detach in _detachActions)
+            {
+                detach();
+            }
+
+            _detachActions.Clear();
+        }
+    }
+}
