1b0358614fdc094220e86950c8a45a7a96a43aa4
orangenjin <takachizu4@gmail.com>
2026-09-06 10:10:05 +0900

    fix

--- PochiPochiEditorPlus/_Utilities/DynamicAccessor.cs
+++ PochiPochiEditorPlus/_Utilities/DynamicAccessor.cs
@@ -6,17 +6,17 @@ namespace PochiPochiEditorPlus._Utilities
     /// <summary>
     /// ドット記法によるアクセスを可能にする。
     /// </summary>
-    public abstract class DynamicAccessor<T> : DynamicObject
+    public sealed class DynamicAccessor<T> : DynamicObject
     {
-        protected Dictionary<string, T> _fields = new Dictionary<string, T>();
+        private Dictionary<string, T> _values = new Dictionary<string, T>();
 
         /// <summary>
         /// 値を登録する。
         /// 辞書のAddの仕様を利用して、上書きを拒否する。
         /// </summary>
-        public virtual void Register(string key, T value)
+        public void Register(string key, T value)
         {
-            _fields.Add(key, value);
+            _values.Add(key, value);
         }
 
         /// <summary>
@@ -24,7 +24,7 @@ namespace PochiPochiEditorPlus._Utilities
         /// </summary>
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
-            if (_fields.TryGetValue(binder.Name, out T value))
+            if (_values.TryGetValue(binder.Name, out T value))
             {
                 result = value;
                 return true;
