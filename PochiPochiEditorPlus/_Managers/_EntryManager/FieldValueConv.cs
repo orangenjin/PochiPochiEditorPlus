a1ba2ca3f1a1928774fe506003215add3590967b
orangenjin <takachizu4@gmail.com>
2026-09-06 17:48:53 +0900

    field value

---
+++ PochiPochiEditorPlus/_Managers/_EntryManager/FieldValueConv.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using PochiPochiEditorPlus._Helpers;
+using PochiPochiEditorPlus._Utilities;
+
+namespace PochiPochiEditorPlus._Managers._EntryManager
+{
+    public static class FieldValueConv
+    {
+        /// <summary>
+        /// byte[](FieldValue.BinaryData)を型Tとして変換する。
+        /// </summary>
+        public static T BytesToModelConv<T>(
+            FieldValueManager fieldValue,
+            int argIndex,
+            CharmapManager charmap)
+        {
+            int entryLength = fieldValue.Lengths.EntryLength;
+            byte[] binaryData = fieldValue.BinaryData;
+
+            // 文字列
+            if (fieldValue.Lengths.AllowedLength > 0)
+            {
+                return (T)Convert.ChangeType
+                    (charmap.BytesToString(binaryData, 0, entryLength),
+                    typeof(T));
+            }
+
+            // StateValueから読み取り方法を取得
+            bool isSigned = fieldValue.State == FieldValueManager.StateValue.Signed;
+            bool isPointer = fieldValue.State == FieldValueManager.StateValue.Pointer;
+
+            long rawValue;
+            switch (entryLength)
+            {
+                case Constants.ByteSize:
+                case Constants.UShortSize:
+                case Constants.UIntSize:
+                    rawValue = IoHelper.ReadBytesAsInt(
+                        binaryData,
+                        0,
+                        entryLength,
+                        isLittleEndian: true,
+                        isSigned: isSigned);
+                    break;
+
+                default:
+                    return default;
+            }
+
+            // 1バイトの特殊処理
+            if (entryLength == Constants.ByteSize)
+            {
+                // ニブル
+                if (fieldValue.ArgCount == AttrKind.NibbleAttr.GetAttrCount())
+                {
+                    int nibbleValue = argIndex == (int)NibbleAttrArgs.HighValueArg
+                        ? (int)((rawValue >> Constants.NibbleShift) & Constants.NibbleMask)
+                        : (int)(rawValue & Constants.NibbleMask);
+
+                    return (T)Convert.ChangeType(nibbleValue, typeof(T));
+                }
+                // ビット
+                else if (fieldValue.ArgCount == AttrKind.BitAttr.GetAttrCount())
+                {
+                    int bitValue = (int)((rawValue >> argIndex) & 1);
+                    return (T)Convert.ChangeType(bitValue, typeof(T));
+                }
+            }
+            // 4バイトの特殊処理
+            else if (entryLength == Constants.UIntSize && isPointer)
+            {
+                return rawValue == 0
+                    ? (T)Convert.ChangeType(Constants.InvalidValue, typeof(T))
+                    : (T)Convert.ChangeType(rawValue - Constants.BaseAddr, typeof(T));
+            }
+
+            return (T)Convert.ChangeType(rawValue, typeof(T));
+        }
+
+        /// <summary>
+        /// 型Tの値をbyte[](FieldValue.BinaryData)に変換する。
+        /// </summary>
+        public static byte[] ModelToBytesConv<T>(
+           T value,
+           FieldValueManager fieldValue,
+           int argIndex,
+           CharmapManager charmap)
+        {
+            int entryLength = fieldValue.Lengths.EntryLength;
+            bool isPointer = fieldValue.State == FieldValueManager.StateValue.Pointer;
+
+            // 戻り値・マージ用
+            byte[] result = new byte[entryLength];
+            Array.Copy(fieldValue.BinaryData, 0, result, 0, entryLength);
+
+            // 文字列
+            if (fieldValue.Lengths.AllowedLength > 0)
+            {
+                var text = Convert.ToString(value);
+
+                // AllowedLength分
+                byte[] bytes = charmap.StringToBytes(
+                    text,
+                    appendTerminator: true,
+                    targetLength: fieldValue.Lengths.AllowedLength);
+
+                // EntryLength分
+                Array.Copy(bytes, 0, result, 0, Math.Min(bytes.Length, entryLength));
+
+                return result;
+            }
+
+            // 1バイトの特殊処理
+            if (entryLength == Constants.ByteSize)
+            {
+                int nibbleSize = AttrKind.NibbleAttr.GetAttrCount();
+                int bitSize = AttrKind.BitAttr.GetAttrCount();
+                byte rawByte = result[0];
+
+                // ニブル（特定のニブルのみ更新）
+                if (fieldValue.ArgCount == nibbleSize)
+                {
+                    byte nibbleValue = Convert.ToByte(value);
+
+                    if (argIndex == (int)NibbleAttrArgs.HighValueArg)
+                    {
+                        // 下位ニブルを残し、上位ニブルに値をセット
+                        rawByte = (byte)((rawByte & ~(Constants.NibbleMask << Constants.NibbleShift))
+                                       | ((nibbleValue & Constants.NibbleMask) << Constants.NibbleShift));
+                    }
+                    else
+                    {
+                        // 上位ニブルを残し、下位ニブルに値をセット
+                        rawByte = (byte)((rawByte & (Constants.NibbleMask << Constants.NibbleShift))
+                                       | (nibbleValue & Constants.NibbleMask));
+                    }
+
+                    result[0] = rawByte;
+                    return result;
+                }
+                // ビット（特定の1ビットのみ更新）
+                else if (fieldValue.ArgCount == bitSize)
+                {
+                    byte bitValue = Convert.ToByte(value);
+
+                    if ((bitValue & 1) == 1)
+                    {
+                        rawByte |= (byte)(1 << argIndex);
+                    }
+                    else
+                    {
+                        rawByte &= (byte)~(1 << argIndex);
+                    }
+
+                    result[0] = rawByte;
+                    return result;
+                }
+            }
+
+            long rawValue;
+            // 4バイト特有の処理
+            if (entryLength == Constants.UIntSize && isPointer)
+            {
+                long tempValue = Convert.ToInt64(value);
+                rawValue = tempValue == Constants.InvalidValue
+                    ? 0
+                    : tempValue + Constants.BaseAddr;
+            }
+            else
+            {
+                rawValue = Convert.ToInt64(value);
+            }
+
+            IoHelper.WriteIntAsBytes(
+                buffer: result,
+                offset: 0,
+                value: rawValue,
+                length: entryLength);
+
+            return result;
+        }
+
+    }
+}
---
+++ PochiPochiEditorPlus/_Managers/_EntryManager/FieldValueManager.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using PochiPochiEditorPlus._Utilities;
+
+namespace PochiPochiEditorPlus._Managers._EntryManager
+{
+    public sealed class FieldValueManager
+    {
+        public int Offset { get; set; }
+        public FieldLength Lengths { get; }
+        public StateValue State { get; }
+        public int ArgCount { get; }
+        public byte[] BinaryData
+        {
+            get
+            {
+                var length = Lengths.EntryLength;
+                byte[] data = new byte[length];
+
+                // RomDataから直接現在のデータを取得
+                Array.Copy(_sharedData.RomData, Offset, data, 0, length);
+                return data;
+            }
+            private set
+            {
+                Array.Copy(value, 0, _sharedData.RomData, Offset, Lengths.EntryLength);
+            }
+        }
+
+        /// <summary>
+        /// エントリー長と入力可能な長さ(StringAttr用)の情報を保持する。
+        /// </summary>
+        public class FieldLength
+        {
+            public int EntryLength { get; set; }
+            public int AllowedLength { get; set; }
+        }
+
+        /// <summary>
+        /// 読み取り方法を定義する。
+        /// </summary>
+        public enum StateValue
+        {
+            Normal,
+            Signed,
+            Pointer
+        }
+
+        private SharedData _sharedData = null;
+        private string _fieldName = null;
+
+        /// <summary>
+        /// DefReaderで読み込んだ定義情報からコンテナを作成する。(ループ処理前提)
+        /// </summary>
+        public FieldValueManager(FieldMetaData metaData, SharedData sharedData)
+        {
+            // 後で使用するので保持
+            _sharedData = sharedData;
+            _fieldName = metaData.Name;
+
+            // 長さを仮置きする
+            Lengths = new FieldLength();
+            Lengths.EntryLength = metaData.Field.GetFieldSize();
+            Lengths.AllowedLength = Constants.InvalidValue;
+
+            // 属性の種類を確認
+            foreach (var attr in metaData.Attrs)
+            {
+                // 高々1つと想定しているのでbreakする
+                switch (attr.Kind)
+                {
+                    case AttrKind.StringAttr:
+                        // 第2引数(AllowedLengthArg)がない場合がある
+                        ArgCount = Math.Min(
+                            attr.Args.Length,
+                            AttrKind.StringAttr.GetAttrCount()); // 引数の最大値と比較
+                        int[] lengths = new int[ArgCount];
+
+                        dynamic config = _sharedData.Config;
+                        for (int i = 0; i < ArgCount; i++)
+                        {
+                            // 属性引数名はiniで定義名と同じ
+                            lengths[i] = config.attr.Args[i];
+                        }
+
+                        // AllowedLengthArgが存在しない場合、同値を入れる
+                        Lengths.EntryLength = lengths[(int)StringAttrArgs.EntryLengthArg];
+                        Lengths.AllowedLength = lengths.Length > 1
+                            ? lengths[(int)StringAttrArgs.AllowedLengthArg]
+                            : lengths[(int)StringAttrArgs.EntryLengthArg];
+                        break;
+
+                    case AttrKind.NibbleAttr:
+                        ArgCount = AttrKind.NibbleAttr.GetAttrCount();
+                        break;
+
+                    case AttrKind.BitAttr:
+                        ArgCount = AttrKind.BitAttr.GetAttrCount();
+                        break;
+                }
+            }
+
+            // 読み取り方法を取得
+            var isSigned = metaData.Field.IsSigned();
+            var isPointer = metaData.Field.IsPointer();
+            State = isPointer
+                ? StateValue.Pointer
+                : (isSigned
+                     ? StateValue.Signed
+                     : StateValue.Normal);
+        }
+
+        /// <summary>
+        /// BinaryDataから型Tの値を取得する。
+        /// </summary>
+        public T GetData<T>(
+            int argIndex = 0,
+            Func<FieldValueManager, int, CharmapManager, T> converter = null)
+        {
+            // 通常の型Tで対応できない特殊処理があれば渡す
+            return converter != null
+                ? converter(this, argIndex, _sharedData.Charmap)
+                : FieldValueConv.BytesToModelConv<T>(this, argIndex, _sharedData.Charmap);
+        }
+
+        /// <summary>
+        /// 型Tの値をBinaryDataに適用する。
+        /// </summary>
+        public void SetData<T>(
+            T rawData,
+            int argIndex = 0,
+            Func<T, FieldValueManager, int, CharmapManager, byte[]> converter = null)
+        {
+            // 通常の型Tで対応できない特殊処理があれば渡す
+            byte[] newBytes = converter != null
+                    ? converter(rawData, this, argIndex, _sharedData.Charmap)
+                    : FieldValueConv.ModelToBytesConv(rawData, this, argIndex, _sharedData.Charmap);
+
+            // 新しいbyte[]を代入
+            BinaryData = newBytes;
+        }
+    }
+}
